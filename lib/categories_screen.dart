847e564aa0d0c905d2f0692e83224d19e1b2e8e3
Carlos Alvarez Moreno <ceam1710@gmail.com>
2019-11-15 15:39:29 -0500

    Creating a Grid & Working with Linear Gradients

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,12 +1,19 @@
+import 'package:deli_meals/category_item.dart';
+import 'package:deli_meals/dummy-data.dart';
 import 'package:flutter/material.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      children: <Widget>[
-        
-      ],
+      children: DUMMY_CATEGORIES
+          .map(
+            (catData) => CategoryItem(
+              catData.title,
+              catData.color,
+            ),
+          )
+          .toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
         childAspectRatio: 3 / 2,
